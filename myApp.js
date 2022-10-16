4bc251b135d210fffac64a67886a669c1b4829bd
lucaseduardolema <lucaseduardolema@gmail.com>
2022-10-16 14:03:51 -0300

    add exercises

--- myApp.js
+++ myApp.js
@@ -1,43 +1,21 @@
-let express = require('express');
-let app = express();
+require('dotenv').config()
+const express = require("express");
+const app = express();
 
-console.log("Hello World");
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-  res.send("Hello Express");
+app.get("/", (_req, res) => {
+  const path = __dirname + "/views/index.html";
+  res.sendFile(path);
 });
 
+app.get('/json', (_req, res) => {
+  let message = 'Hello json'
+  console.log(process.env.MESSAGE_STYLE);
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase()
+  }
+  res.json({message})
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
